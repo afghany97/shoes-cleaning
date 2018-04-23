ef5d3908005115a95353d36256b5dd141ba75c60
Muhammad Reda <muhammadreda97@gmail.com>
2018-04-23 17:11:18 +0200

    delete column from users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('mobile');
-            $table->string('token')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
